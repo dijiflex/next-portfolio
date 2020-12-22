04b52ff906f86adcf4bea59d40c7b8bef5aec858
dijiflex <dijiflex@gmail.com>
2020-12-22 23:54:07 +0300

    Finalized the app development

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
 
 
-import Link from 'next/link';
 import Layout from '../components/Layout';
 const Index = () => (
 <Layout title='Home'>
-    <Link href='/about'>
-      <a >Go to About Page</a>
-     </Link>
+    
     <div>Welcome To Next.js</div>
 </Layout>
     
--- pages/post.js
+++ pages/post.js
@@ -6,7 +6,7 @@ import { withRouter } from 'next/router';
 
 const Post = ({ router }) => (
  <Layout title={router.query.title}>
-     <p>
+     <p style={{width : "80vw"}}>
          Lorem is the thing that is working for us here
          Lorem is the thing that is working for us here
          Lorem is the thing that is working for us hereLorem is the thing that is working for us hereLorem is the thing that is working for us here
